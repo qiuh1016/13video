a4b5d33cccec2a242eb7b6b9a6b848eb070e7392
Qiu Hong <qiuh1016@126.com>
2018-08-19 13:32:39 +0800

    1

--- routes/controller/site.js
+++ routes/controller/site.js
@@ -227,3 +227,22 @@ exports.upload = async (ctx, next) => {
     filename: ctx.req.file.filename // 返回文件名
   }
 }
+
+const multer = require('koa-multer');//加载koa-multer模块
+//文件上传
+//配置
+var storage = multer.diskStorage({
+  //文件保存路径
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads/')
+  },
+  //修改文件名称
+  filename: function (req, file, cb) {
+    var fileFormat = (file.originalname).split(".");
+    cb(null, Date.now() + "." + fileFormat[fileFormat.length - 1]);
+  }
+})
+//加载配置
+var uploadMulter = multer({ storage: storage });
+
+exports.uploadMulter = uploadMulter;
